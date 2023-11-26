387f0e2f343bac4fa69218de5e0bf98df738ff91
petratalvensaari <petra.eveliina01@gmail.com>
2023-11-26 18:51:36 +0200

    Formi näkyy

--- Components/Booklist.js
+++ Components/Booklist.js
@@ -1,30 +1,62 @@
-import React from "react";
-import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Button, StyleSheet, TouchableOpacity, Text, TextInput, } from "react-native";
 
-function Booklist({ navigation }) {
+// function Booklist({ navigation }) { //
 
-  return (
+const Booklist = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [year, setYear] = useState("");
+  const [display, setDisplay] = useState(false);
+
+  const resetForm =()=>{
+    setDisplay(false);
+    setName("");
+    setAuthor("");
+    setYear("")
+  }
+
+
+return (
+  
+  <View style={styles.container}>
     
-    <View style={styles.container}>
-      
-      <Text>Lista lukemista kirjoistani</Text>
+    <TextInput
+    style={styles.textInput}
+    placeholder="Enter book name"
+    onChangeText={(text)=>{setName(text)}}
+    value={name}
+    />
+    <TextInput
+    style={styles.textInput}
+    placeholder="Enter author"
+    onChangeText={(text)=>{setAuthor(text)}}
+    value={author}
+    />
+    <TextInput
+    style={styles.textInput}
+    placeholder="Enter year"
+    onChangeText={(text)=>{setYear(text)}}
+    value={year}
+    />
 
-      <Button title="Lisää uusi kirja" size="s" backgroundColor="#ffb6c1"
-        onPress={() => navigation.navigate('Uuden kirjan lisäys')}
-      />
 
-    </View>
-    
-  );
+    <Button title="Lisää uusi kirja" size="s" backgroundColor="#ffb6c1"
+      onPress={() => navigation.navigate('Uuden kirjan lisäys')}
+    />
+
+  </View>
+  
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#faf0e6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  backgroundColor: '#faf0e6',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
 });
 
 export default Booklist;
